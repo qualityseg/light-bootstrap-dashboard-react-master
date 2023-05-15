6644b115a23aa1597717f1be412212ef294d3da8
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-15 16:37:55 -0300

    5000

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -21,10 +21,11 @@ function Dashboard() {
     <>
       <center><iframe title="OS" width="900" height="550" src="https://app.powerbi.com/view?r=eyJrIjoiYmMyYjdjYTItNDcyMi00ZTY4LTlkOWQtZDBhOGZmY2JlOWM0IiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9" frameborder="0" allowFullScreen="true"></iframe></center>
 
-      <center><iframe title="OS" width="900" height="550" src="https://analytics.zoho.com/open-view/2737732000000003453" frameborder="0" allowFullScreen="true"></iframe></center>
+      <center><iframe title="OS" width="900" height="550" src="https://analytics.zoho.com/open-view/2737732000000003416" frameborder="0" allowFullScreen="true"></iframe></center>
 
     </>
   );
 }
+
 
 export default Dashboard;
