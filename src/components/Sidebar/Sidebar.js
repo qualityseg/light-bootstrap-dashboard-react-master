67c7df9304e8f58f94fff08229c7a53bd07e76c2
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-16 15:33:07 -0300

    green

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -29,7 +29,7 @@ function Sidebar({ color, image, routes }) {
     return location.pathname.indexOf(routeName) > -1 ? "active" : "";
   };
   return (
-    <div className="sidebar" data-image={image} data-color='#00ac5a'>
+    <div className="sidebar" data-image={image} data-color='green'>
       <div
         className="sidebar-background"
         style={{
