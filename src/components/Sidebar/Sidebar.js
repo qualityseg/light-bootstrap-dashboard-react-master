362465390cb825032fe56bf1e08a83ab4fc42e0d
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-16 16:23:57 -0300

    eso

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -43,7 +43,7 @@ function Sidebar({ color, image, routes }) {
             className="simple-text logo-mini mx-1"
           >
             <div className="logo-img">
-              <img src={require("assets/img/reactlogo.png")} alt="..." style={{ width: "100%", height: "100%" }} />
+              <img src={require("assets/img/reactlogo.png")} alt="..." style={{ width: "400%", height: "400%" }} />
             </div>
           </a>
           <a className="simple-text" href="">
