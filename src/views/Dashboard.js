e9a6674ef67c7778ce4a77e424ac238aaf869335
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-18 12:52:47 -0300

    ideia

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -19,7 +19,7 @@ import {
 function Dashboard() {
   return (
     <>
-      <center><iframe title="OS 20" width="900" height="436" src="https://app.powerbi.com/view?r=eyJrIjoiNDg2NzAxYzUtMzhhOC00OWQ5LWE4ZDUtOTRlNTA0YmFlYWE4IiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9" frameborder="0" allowFullScreen="true"></iframe></center>
+      <center><iframe title="OS 20" width="900" height="536" src="https://app.powerbi.com/view?r=eyJrIjoiNDg2NzAxYzUtMzhhOC00OWQ5LWE4ZDUtOTRlNTA0YmFlYWE4IiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9" frameborder="0" allowFullScreen="true"></iframe></center>
 
 
       <center><iframe title="OS" width="900" height="550" src="https://analytics.zoho.com/open-view/2737732000000028463" frameborder="0" allowFullScreen="true"></iframe></center>
