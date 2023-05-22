33efda1b3ea64cdc86de814942a98443f86b0403
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-22 10:38:46 -0300

    nova dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,43 +1,194 @@
-import React from "react";
-import ChartistGraph from "react-chartist";
-// react-bootstrap components
-import {
-  Badge,
-  Button,
-  Card,
-  Navbar,
-  Nav,
-  Table,
-  Container,
-  Row,
-  Col,
-  Form,
-  OverlayTrigger,
-  Tooltip,
-} from "react-bootstrap";
+import React, { useState } from 'react';
+import { Col, Card as BootstrapCard, Row, Container } from 'react-bootstrap';
+import './styles.dashboard.scss';
 
-function Dashboard() {
+const Dashboard = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [content, setContent] = useState(null);
+
+  const handleOpenClick = (contentType) => {
+    setIsExpanded(!isExpanded);
+
+    // Define o conteúdo da coluna expandida com base no tipo de conteúdo
+    switch (contentType) {
+      case 'realizarOS':
+        setContent(
+          <iframe
+            title="OS 20"
+            width="900"
+            height="536"
+            src="https://forms.zohopublic.com/juliana15/form/ComercialQualitySegEngenhariadeSeguranaeMedicinado/formperma/iKxe7-5DD6rWrFM_duryGABo0x1oPTqqlnpafECN11w"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        );
+        break;
+      case 'visualizarOS':
+        setContent(
+          <iframe
+            title="OS"
+            width="900"
+            height="550"
+            src="https://app.powerbi.com/view?r=eyJrIjoiMDhhMGVhM2EtYzJkNy00M2I4LWFkMWMtNDk5ZWUyYzE4YmJjIiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        );
+        break;
+      case 'controleFrota':
+        setContent(
+          <div>
+            <h1>Controle de Frota</h1>
+            <p>Conteúdo específico para Controle de Frota</p>
+          </div>
+        );
+        break;
+      case 'verGraficos':
+        setContent(
+          <div>
+            <center>
+              <iframe
+                title="Graficos"
+                width="900"
+                height="536"
+                src="https://app.powerbi.com/view?r=eyJrIjoiNDg2NzAxYzUtMzhhOC00OWQ5LWE4ZDUtOTRlNTA0YmFlYWE4IiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </center>
+            <p>Conteúdo específico para Ver Gráficos</p>
+          </div>
+        );
+        break;
+      default:
+        setContent(null);
+        break;
+    }
+  };
+
+  const handleCloseClick = () => {
+    setIsExpanded(false);
+    setContent(null);
+  };
+
   return (
     <>
-      <center><iframe title="OS 20" width="900" height="536" src="https://app.powerbi.com/view?r=eyJrIjoiNDg2NzAxYzUtMzhhOC00OWQ5LWE4ZDUtOTRlNTA0YmFlYWE4IiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9" frameborder="0" allowFullScreen="true"></iframe></center>
+      <Container fluid>
+        <Row>
+        <Col lg="3" sm="6" className="text-center">
+          <BootstrapCard className="card-stats">
+            <BootstrapCard.Body>
+              <div className="d-flex align-items-center justify-content-center">
+                <BootstrapCard.Title as="h4" style={{ fontSize: '15px' }}>Realizar OS</BootstrapCard.Title>
+              </div>
+            </BootstrapCard.Body>
+            <BootstrapCard.Footer>
+              <hr />
+              <div className="stats">
+                {isExpanded ? (
+                  <button className="btn btn-link" onClick={handleCloseClick}>
+                    <span className="ml-1">Fechar</span>
+                  </button>
+                ) : (
+                  <button className="btn btn-link" onClick={() => handleOpenClick('realizarOS')}>
+                    <span className="ml-1">Abrir</span>
+                  </button>
+                )}
+              </div>
+            </BootstrapCard.Footer>
+          </BootstrapCard>
+        </Col>
 
-      <p>
 
-      </p>
-      <p></p>
-      
-      <center><iframe title="OS" width="900" height="550" src="https://app.powerbi.com/view?r=eyJrIjoiMDhhMGVhM2EtYzJkNy00M2I4LWFkMWMtNDk5ZWUyYzE4YmJjIiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9" frameborder="0" allowFullScreen="true"></iframe></center>
-      <p>
+        <Col lg="3" sm="6" className="text-center">
+          <BootstrapCard className="card-stats">
+            <BootstrapCard.Body>
+              <div className="d-flex align-items-center justify-content-center">
+                <BootstrapCard.Title as="h4" style={{ fontSize: '15px' }}>OS em Excel</BootstrapCard.Title>
+              </div>
+            </BootstrapCard.Body>
+            <BootstrapCard.Footer>
+              <hr />
+              <div className="stats">
+                {isExpanded ? (
+                  <button className="btn btn-link" onClick={handleCloseClick}>
+                    <span className="ml-1">Fechar</span>
+                  </button>
+                ) : (
+                  <button className="btn btn-link" onClick={() => handleOpenClick('realizarOS')}>
+                    <span className="ml-1">Abrir</span>
+                  </button>
+                )}
+              </div>
+            </BootstrapCard.Footer>
+          </BootstrapCard>
+        </Col>
 
-</p>
-<p></p>
+        <Col lg="3" sm="6" className="text-center">
+          <BootstrapCard className="card-stats">
+            <BootstrapCard.Body>
+              <div className="d-flex align-items-center justify-content-center">
+                <BootstrapCard.Title as="h4" style={{ fontSize: '15px' }}>PlaceHolder Text</BootstrapCard.Title>
+              </div>
+            </BootstrapCard.Body>
+            <BootstrapCard.Footer>
+              <hr />
+              <div className="stats">
+                {isExpanded ? (
+                  <button className="btn btn-link" onClick={handleCloseClick}>
+                    <span className="ml-1">Fechar</span>
+                  </button>
+                ) : (
+                  <button className="btn btn-link" onClick={() => handleOpenClick('realizarOS')}>
+                    <span className="ml-1">Abrir</span>
+                  </button>
+                )}
+              </div>
+            </BootstrapCard.Footer>
+          </BootstrapCard>
+        </Col>
 
-<center><iframe title="OS" width="900" height="550" src="https://analytics.zoho.com/open-view/2737732000000028463" frameborder="0" allowFullScreen="true"></iframe></center>
-      
 
+        <Col lg="3" sm="6" className="text-center">
+          <BootstrapCard className="card-stats">
+            <BootstrapCard.Body>
+              <div className="d-flex align-items-center justify-content-center">
+                <BootstrapCard.Title as="h4" style={{ fontSize: '15px' }}>Graficos</BootstrapCard.Title>
+              </div>
+            </BootstrapCard.Body>
+            <BootstrapCard.Footer>
+              <hr />
+              <div className="stats">
+                {isExpanded ? (
+                  <button className="btn btn-link" onClick={handleCloseClick}>
+                    <span className="ml-1">Fechar</span>
+                  </button>
+                ) : (
+                  <button className="btn btn-link" onClick={() => handleOpenClick('verGraficos')}>
+                    <span className="ml-1">Abrir</span>
+                  </button>
+                )}
+              </div>
+            </BootstrapCard.Footer>
+          </BootstrapCard>
+        </Col>
+
+
+          {/* Restante das colunas omitidas por brevidade */}
+        </Row>
+        <Row>
+          <Col lg="12" className={`text-center expanded ${isExpanded ? 'show' : ''}`}>
+            <BootstrapCard className="card-stats">
+              <div className="iframe-container">
+                {isExpanded && content}
+              </div>
+            </BootstrapCard>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
-}
-
+};
 
 export default Dashboard;
+
